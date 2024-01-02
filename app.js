064bc2761af0f21b560e2cb445ef9ff260b1b79c
Vilelax <gabrielvsn@gmail.com>
2024-01-02 13:37:12 -0300

    Added CORS headers

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use('/products', productRouter);
 app.use('/categories', categoryRouter);
 
